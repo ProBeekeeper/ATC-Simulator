e1603fd83560138b74f470e74ba93a99e3b041e5
xTeStX1 <1234567890testx@gmail.com>
2026-04-09 02:25:00 +0800

    Delete Task 2/main.py

--- Task 2/main.py
+++
@@ -1,8 +0,0 @@
-from game import Game
-
-# ==========================================
-# Main Program Entry Point
-# ==========================================
-if __name__ == "__main__":
-    game = Game()
-    game.run()
