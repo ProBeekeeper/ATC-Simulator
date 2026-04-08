217118b3a134ff16be21752e60dfdf9d0d25ee6c
xTeStX1 <1234567890testx@gmail.com>
2026-04-09 02:32:48 +0800

    Add files via upload

---
+++ Task 2/dsu.py
@@ -0,0 +1,33 @@
+
+# Core Data Structure: Disjoint Set Union (DSU)
+
+class DSU:
+
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, i):
+       
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+       
+        root_i = self.find(i)
+        root_j = self.find(j)
+
+        if root_i == root_j:
+            return False  # Already connected
+
+        # Union by rank
+        if self.rank[root_i] < self.rank[root_j]:
+            self.parent[root_i] = root_j
+        elif self.rank[root_i] > self.rank[root_j]:
+            self.parent[root_j] = root_i
+        else:
+            self.parent[root_j] = root_i
+            self.rank[root_i] += 1
+
+        return True
